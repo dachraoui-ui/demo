3267389ca290ddb3591d056d5a72395c1d6f6356
ahmed dacharoui <dachraouia193@gmail.com>
2024-07-13 21:26:54 +0100

    add model and club class

---
+++ src/main/java/org/newapp/demo3/Model/Club.java
@@ -0,0 +1,26 @@
+package org.newapp.demo3.Model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Table(name = "club")
+public class Club {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    private String name;
+    private String location;
+    private String stadium;
+    private String coach;
+    private String league;
+    private String country;
+
+}
