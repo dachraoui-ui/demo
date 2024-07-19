2b27fc03b9bc66411d3ba08f9b1ed8514473cd21
ahmed dacharoui <dachraouia193@gmail.com>
2024-07-19 20:40:54 +0100

    addd

--- src/main/java/org/newapp/demo3/Controller/ClubController.java
+++ src/main/java/org/newapp/demo3/Controller/ClubController.java
@@ -5,9 +5,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
-
+@RequestMapping("/admin")
 @Controller
 public class ClubController {
     private final ClubService clubService;
@@ -23,4 +24,11 @@ public class ClubController {
         model.addAttribute("clubs", clubs);
         return "club_list";
     }
+    @GetMapping("/club/add")
+    public String addClub(Model model){
+        Club club = new Club();
+        model.addAttribute("club",club);
+        return "add_Club";
+    }
+
 }
