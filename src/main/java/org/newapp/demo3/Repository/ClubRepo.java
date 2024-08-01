0151b6966f6fbe13e4eb486cc9f9cadf00efe4f1
ahmed dacharoui <dachraouia193@gmail.com>
2024-08-01 12:51:45 +0100

    add query

--- src/main/java/org/newapp/demo3/Repository/ClubRepo.java
+++ src/main/java/org/newapp/demo3/Repository/ClubRepo.java
@@ -2,9 +2,14 @@ package org.newapp.demo3.Repository;
 
 import org.newapp.demo3.Model.Club;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
+import java.util.List;
+import java.util.Optional;
 
 
 public interface ClubRepo extends JpaRepository<Club, Integer> {
-
+    @Query("select c from Club where c.name like concat('%',:query,'%')");
+    Optional<Club> findByName(String name);
+    List<Club> searchByName(String query);
 }
